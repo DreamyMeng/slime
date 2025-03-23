9965d98c3a5c73316527068baac5c5b362a0aa1b
孟无岐 <dragon01032@163.com>
2025-03-23 11:38:58 +0800

    配置

--- src/Main.ts
+++ src/Main.ts
@@ -1,9 +1,3 @@
-const { regClass, property } = Laya;
-
-@regClass()
-export class Main extends Laya.Script {
-
-    onStart() {
-        console.log("Game start");
-    }
+export async function main() {
+    console.log("Hello, world!");
 }

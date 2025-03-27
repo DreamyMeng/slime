f0b87904262fb067f4d13e76ac44f178c868b63e
孟无岐 <dragon01032@163.com>
2025-03-27 09:26:45 +0800

    角色表

--- src/Main.ts
+++ src/Main.ts
@@ -3,16 +3,16 @@ import { Tables } from "./table/schema";
 export async function main() {
     console.log("Hello, world!");
 
-    let table: Tables;
+    // let table: Tables;
 
-    await Laya.loader.load("resources/json/tbdemo.json").then((json: Laya.TextResource) => {
-        table = new Tables(() => {
-            return json.data;
-        });
-        console.log('加载配置表成功')
-    });
+    // await Laya.loader.load("resources/json/tbdemo.json").then((json: Laya.TextResource) => {
+    //     table = new Tables(() => {
+    //         return json.data;
+    //     });
+    //     console.log('加载配置表成功')
+    // });
 
-    let hero = table.Tbdemo.get(1001);
+    // let hero = table.Tbdemo.get(1001);
 
-    console.log(hero.name);
+    // console.log(hero.name);
 }
